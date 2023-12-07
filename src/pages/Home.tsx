7154b2ec34b4ab9b2f8da06f74cea211f4cb28a9
Lucas Bateson <lucas.bateson@gmail.com>
2023-12-07 21:01:47 +0100

    Modularize code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,42 +1,68 @@
-// Home.jsx
-import { useEffect, useState } from 'react';
+// Home.tsx
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getStationData, StationData } from '../api';
 import './Home.css';
 
-export default function Home() {
+interface StationButtonProps {
+  station: StationData;
+  onClick: (id: number) => void;
+}
+
+const StationButton: React.FC<StationButtonProps> = ({ station, onClick }) => (
+  <button
+    key={station.groupId}
+    className="station-button"
+    onClick={() => onClick(station.groupId)}
+    aria-label={`Station ${station.station}`}
+  >
+    {station.station}
+    <span className="material-symbols-outlined float-right">
+      arrow_forward_ios
+    </span>
+  </button>
+);
+
+const Home: React.FC = () => {
   const [stations, setStations] = useState<StationData[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
   const navigate = useNavigate();
 
   useEffect(() => {
     getStationData()
-      .then(setStations)
+      .then(data => {
+        setStations(data);
+        setLoading(false);
+      })
       .catch(error => {
         console.error('Failed to fetch station data:', error);
+        setError('Error fetching station data');
+        setLoading(false);
       });
   }, []);
 
-  const handleStationClick = (id: number) => {
+  const handleStationClick = (id: number): void => {
     navigate(`/booking/${id}`);
   };
+
+  if (loading) return <p>Loading stations...</p>;
+  if (error) return <p>{error}</p>;
 
   return (
     <div className="home">
       <h1>Velg Stasjon</h1>
       <div className="station-grid">
         {stations.map((station) => (
-          <button
+          <StationButton
             key={station.groupId}
-            className="station-button"
-            onClick={() => handleStationClick(station.groupId)}
-          >
-            {station.station}
-            <span className="material-symbols-outlined float-right">
-              arrow_forward_ios
-            </span>
-          </button>
+            station={station}
+            onClick={handleStationClick}
+          />
         ))}
       </div>
     </div>
   );
-}
+};
+
+export default Home;
